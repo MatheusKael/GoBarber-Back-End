7aa78744e85931e4dcdcd9bef03d95a34a0e245f
MatheusKael <matheuskael.silva@gmail.com>
2020-08-26 18:40:25 -0400

    Feat: Database

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router, Request } from 'express'
+import { parseISO} from 'date-fns'
+import { getCustomRepository } from 'typeorm'
+
+import AppointmentRepository from '../repositories/AppointmentsRepository'
+import CreateAppointment from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router()
+
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository)
+  const appointments = await appointmentRepository.find();
+
+  return response.json(appointments)
+})
+
+appointmentsRouter.post('/', async (request: Request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+  const parsedDate = parseISO(date)
+
+  const createAppointment = new CreateAppointment();
+
+  const appointment = await createAppointment.execute({date: parsedDate, provider})
+
+
+  return response.json( appointment )
+  }catch(err){
+    return response.status(400).json({error: err.message})
+  }
+});
+
+
+export default appointmentsRouter;
