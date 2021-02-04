28a352345b2d8de8c1fabd7b7b61c7ad79a75740
MatheusKael <matheuskael.silva@gmail.com>
2021-02-04 19:10:50 -0400

    Feat: listing available days

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,3 +1,4 @@
+import { getDaysInMonth, getDate } from 'date-fns';
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 
@@ -34,9 +35,25 @@ class ListProviderMonthAvailabilityService {
       }
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
