59d5bfef60fc763b1d683cce3f92095f003edf20
MatheusKael <matheuskael.silva@gmail.com>
2021-02-18 13:37:47 -0400

    Fix: ListProviderMonthAvailability

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -43,7 +43,7 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
-      const compareDate = new Date(day, month - 1, year, 23, 59, 59);
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
